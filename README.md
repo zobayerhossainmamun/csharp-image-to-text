9ca7579d9c07a255cbe7d1791bc12fea15e76e45
Md Zobayer Hossain Mamun <zobayerhossainmamun@gmail.com>
2023-12-11 13:08:30 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+<div align="center">
+  <h2>Get Text From Image in C#</h2>
+</div>
+
+<p align="center">This is an C# Console Application. it used to get all text from image with Tesseract OCR Engine.</p>
+
+## Description
+
+In this application, you can extract all text from a image file with Tesseract OCR Engine.
+
+## Tesseract
+
+Tesseract is an optical character recognition engine for various operating systems. It is free software, released under the Apache License. Originally developed by Hewlett-Packard as proprietary software in the 1980s, it was released as open source in 2005 and development has been sponsored by Google since 2006.
+
+## Install Tesseract on windows
+
+```bash
+# Open this link and download setup file based on your system requirements.
+https://digi.bib.uni-mannheim.de/tesseract/
+```
---
+++ csharp-image-to-text/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace csharp_image_to_text
+{
+    internal class Program
+    {
+        private static readonly string BIN_PATH = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        private static readonly string TesseractPATH = "C:\\Program Files (x86)\\Tesseract-OCR";
+
+
+        private static string ConvertImageToText(string path)
+        {
+            var service = new TesseractService(TesseractPATH, "eng", $"{TesseractPATH}\\tessdata", $"{BIN_PATH}\\output");
+            var stream = File.OpenRead(path);
+            return service.GetText(stream);
+        }
+
+        static void Main(string[] args)
+        {
+            string text = ConvertImageToText($"{BIN_PATH}\\image.png");
+            Console.WriteLine(text);
+
+            Console.WriteLine("\nEnter any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
